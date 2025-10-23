6d959b67a67219fac5b5749060ac8de933a413fa
unknown <quocbo303@gmail.com>
2025-10-23 17:33:13 +0700

    fisrtcommit

---
+++ Online_Meeting/SignalingServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿//# EF Core DbContext
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Meeting.SignalingServer.Data
+{
+    internal class ApplicationDbContext
+    {
+    }
+}
